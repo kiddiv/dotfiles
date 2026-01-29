f4e93cfcd68b50db7c18113f8c681aa803d6a8c8
kiddiv <egorkathebestlordtop1gg@gmail.com>
2026-01-29 03:49:05 -0500

    Fix

--- .chezmoiscripts/run_once_install-zsh-setup.sh
+++ .chezmoiscripts/run_once_install-zsh-setup.sh
@@ -1,44 +1,29 @@
-
 #!/bin/bash
 
 
 # Install Oh My Zsh
-
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
-
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
 fi
 
 # Install zsh-autosuggestions
-
 if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
-
   git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
 fi
 
 # Install zsh-syntax-highlighting
-
 if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
-
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
 fi
 
 # Install fzf-tab
-
 if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab" ]; then
-
   git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
-
 fi
 
 # Install Powerlevel10k theme
-
 if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
-
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-
 fi
 
+echo "Done, restart your shell: exec zsh"
